a5f2cf0832a4c1bab969287c4483d031f608f1f5
Emely Kauane <emely.kauane@academico.ifpb.edu.br>
2025-04-22 11:39:22 -0300

    Texto que representa as mudanças realizadas

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Bem vindo ao nosso site!</Text>
     </View>
   );
 }
